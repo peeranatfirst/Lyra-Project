88739351c3895b76e673b1394a3c0589fa1c301b
Anuwat <fluketmk@gmail.com>
2017-04-27 02:26:54 +0700

    create detail-challenge component in my-challenge

---
+++ src/app/my-challenge/detail-challenge/detail-challenge.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-challenge',
+  templateUrl: './detail-challenge.component.html',
+  styleUrls: ['./detail-challenge.component.css']
+})
+export class DetailChallengeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
