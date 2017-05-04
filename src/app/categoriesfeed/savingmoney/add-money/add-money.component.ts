62df07d25209b8c561ad7faef15952521736a543
pimpattcha <patcha-story@hotmail.com>
2017-05-04 12:25:29 +0700

    install angular2-modal

--- src/app/categoriesfeed/savingmoney/add-money/add-money.component.ts
+++ src/app/categoriesfeed/savingmoney/add-money/add-money.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { AddMoneyService } from "app/services/add-money.service";
 import { routing } from '../../../app.routing';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { FirebaseService } from "app/services/firebase.service";
+import { Overlay } from 'angular2-modal';
+import { Modal } from 'angular2-modal/plugins/bootstrap';
 
 @Component({
   selector: 'app-add-money',
@@ -21,9 +23,12 @@ export class AddMoneyComponent implements OnInit {
     private firebaseService: FirebaseService,
     private router: Router,
     private route: ActivatedRoute,
-    private AddMoneyService: AddMoneyService
-  )
-  { }
+    private AddMoneyService: AddMoneyService,
+    private overlay: Overlay,
+    private vcRef: ViewContainerRef,
+    private modal: Modal  ){
+      overlay.defaultViewContainer = vcRef;
+     }
 
   ngOnInit() {
     //Get id
@@ -52,7 +57,7 @@ export class AddMoneyComponent implements OnInit {
     })
 
     let toAchieved = totalAmount - currentMoney;
-    if (toAchieved > 0) {
+    /*if (toAchieved > 0) {
       if (addmoney <= toAchieved) {
         this.AddMoneyService.addMoney(addmoney, this.id);
 
@@ -63,10 +68,17 @@ export class AddMoneyComponent implements OnInit {
     }else{
       let msg = "";
       return 
-    }
+    }*/
+    this.confirmAlertBox();
 
+    //this.router.navigate(['/detailmychallenge/' + this.id])
+  }
 
-    this.router.navigate(['/detailmychallenge/' + this.id])
+  confirmAlertBox(){
+    this.modal.alert().size('sm').title('this is title naja').body(`
+            <h4>Alert is a classic (title/body/footer) 1 button modal window that 
+            does not block.</h4>`).open();
+    
   }
 
 
