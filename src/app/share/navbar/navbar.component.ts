8a3bc997680906a9374fac2a7809dbb671b9d8ee
Anuwat <fluketmk@gmail.com>
2017-08-23 00:32:05 +0700

    navbar

--- src/app/share/navbar/navbar.component.ts
+++ src/app/share/navbar/navbar.component.ts
@@ -40,17 +40,14 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
 
     $(document).ready(function () {
-      $(".fa-bars2").hide();
-
-      $(".fa-bars1").click(function () {
+      $(".fa-bars").click(function () {
         $(".menu").removeClass('menuClose');
         $(".menu").addClass('menuOpen');
 
         $(".mainClose").addClass('mainOpen');
         $(".mainOpen").removeClass('mainClose');
 
-        $(".fa-bars1").hide(500);
-        $(".fa-bars2").show(500);
+        $(".fa-bars").hide(500);
         $(".navbar-brand").hide(500);
         $(".fa-times").show(500);
 
@@ -61,20 +58,7 @@ export class NavbarComponent implements OnInit {
         document.getElementById('notiBellId').style.display = "none";
         document.getElementById('notiBellOId').style.display = "";
       });
-
-      $(".fa-bars2").click(function () {
-        $(".menu").addClass('menuClose');
-        $(".menu").removeClass('menuOpen');
-
-        $(".mainOpen").addClass('mainClose');
-        $(".mainClose").removeClass('mainOpen');
-
-        $(".fa-times").hide(500);
-        $(".fa-bars2").hide(500);
-        $(".fa-bars1").show(500);
-        $(".navbar-brand").show(500);
-      });
-
+      
       $(".fa-times").click(function () {
         $(".menu").addClass('menuClose');
         $(".menu").removeClass('menuOpen');
