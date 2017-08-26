b7634f7f99c26fe14cb2741d9cd78caf7a1c7fef
pimpattcha <patcha-story@hotmail.com>
2017-08-26 21:52:20 +0700

    set up displayname and photo for signup with email

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -16,22 +16,38 @@ export class SignupComponent implements OnInit {
   state: string = '';
   error: any;
 
+  displayname: any;
   constructor(public af: AngularFire,private router: Router) {
 
   }
   
   onSubmit(formData) {
-    if(formData.valid) {
+   /* if(formData.valid) {
       console.log(formData.value);
       this.af.auth.createUser({
         email: formData.value.email,
         password: formData.value.password
-        }).then(
+      }).then(
         (success) => {
-        this.router.navigate(['/'])
+        this.router.navigate(['/members'])
       }).catch(
         (err) => {
         this.error = err;
+      })
+    }*/
+
+    if(formData.valid){
+      this.displayname = formData.value.displayname;
+      firebase.auth().createUserWithEmailAndPassword(formData.value.email, formData.value.password)
+      .then(()=>{
+        firebase.auth().currentUser.updateProfile({
+          displayName: this.displayname,
+          photoURL: "https://firebasestorage.googleapis.com/v0/b/lyra-24b71.appspot.com/o/user%20(1).png?alt=media&token=d6607806-e826-451a-a25f-471f4f01eebf"
+        })
+      }).then(()=>{
+        firebase.auth().signOut()
+      }).then(()=>{
+        this.router.navigate(['/login-email']);
       })
     }
   }
