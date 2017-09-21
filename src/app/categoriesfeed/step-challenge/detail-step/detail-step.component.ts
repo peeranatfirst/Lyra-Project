ef55703680ab4a1b00087b38dbbbcac0322a3975
pimpattcha <patcha-story@hotmail.com>
2017-09-21 19:05:10 +0700

    retrive data for step challenge

--- src/app/categoriesfeed/step-challenge/detail-step/detail-step.component.ts
+++ src/app/categoriesfeed/step-challenge/detail-step/detail-step.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { routing } from '../../../app.routing';
+import { FirebaseService } from 'app/services/firebase.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import * as firebase from 'firebase';
+import { DatetimestampService } from 'app/services/datetimestamp.service';
+import { GetUserInfoService } from "app/services/get-user-info.service";
+import { Location } from "@angular/common";
+import $ from 'jquery';
 
 @Component({
   selector: 'app-detail-step',
@@ -7,9 +15,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailStepComponent implements OnInit {
 
-  constructor() { }
+  id: any;
+  stepChaDetail: any;
+  datestamp: any;
+  timestamp: any;
+  imageUrl: any;
+  info: any;
+  displayName: any;
+  ownerPhoto: any;
+  steps: any;
+
+  uid: any;
+  isOwner: any;
+
+  constructor(private firebaseService: FirebaseService,
+    private routing: Router,
+    private route: ActivatedRoute,
+    private dt: DatetimestampService,
+    private userinfo: GetUserInfoService,
+    private location: Location) { }
 
   ngOnInit() {
+    
+    //open comment
+    $(document).ready(function () {
+      $("#commentBt").click(function () {
+        document.getElementById('card-comment').style.display = '';
+      })
+
+      $("#closeBt").click(function () {
+        document.getElementById('transaction').style.display = 'none';
+      })
+    });
+
+    // Get id
+    this.id = this.route.snapshot.params['id'];
+
+    this.firebaseService.getChecklistChallengeDetails(this.id).subscribe(step => {
+      this.stepChaDetail = step;
+
+      this.datestamp = this.dt.getDatestamp(this.stepChaDetail.datetimestamp);
+      this.timestamp = this.dt.getTimestamp(this.stepChaDetail.datetimestamp);
+
+      const storageRef = firebase.storage().ref();
+      const spaceRef = storageRef.child(step.path);
+      storageRef.child(step.path).getDownloadURL().then((url) => {
+        // Set image url
+        this.imageUrl = url;
+      });
+
+      this.uid = this.stepChaDetail.owner;
+      var currentUser = firebase.auth().currentUser.uid;
+      if(this.uid == currentUser){
+        this.isOwner = true;
+      }else{
+        this.isOwner = false;
+      }
+
+      this.userinfo.getUserInfo(this.uid).subscribe(info => {
+        this.info = info;
+        this.displayName = this.info.name;
+        this.ownerPhoto = this.info.pathPhoto;
+      });
+    });
+
+    this.firebaseService.getStepsOfStepChallenge(this.id).subscribe(steps => {
+      this.steps = steps;
+    });
+
+
+  }
+
+  onBack() {
+    this.location.back();
+  }
+
+  onDeleteChallenge(){
+    if (confirm("Are you sure to delete?")) {
+      firebase.database().ref('/AllChallenge/'+this.id).remove();
+      this.routing.navigate(['stepchallenge/']);
+    }
   }
 
 }
