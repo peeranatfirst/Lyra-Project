1492779d099e8a93aef3d3143890122f7977e388
Peeranat <ffirstpl@gmail.com>
2017-10-29 18:50:16 +0700

    add comment service

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import * as firebase from 'firebase';
+import { FirebaseService } from 'app/services/firebase.service';
+
+@Injectable()
+export class CommentService {
+
+  constructor() { }
+
+}
