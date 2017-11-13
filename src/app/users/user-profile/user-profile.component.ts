80ea1099da3ec6cdfb03aa00432d4397d665ecf6
Anuwat <fluketmk@gmail.com>
2017-11-13 11:14:29 +0700

    user profile

--- src/app/users/user-profile/user-profile.component.ts
+++ src/app/users/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,6 +11,23 @@ export class UserProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function () {
+      $('#MyChallenge-tab').addClass('active');
+
+      $("#MyChallenge-tab").click(function () {
+        $('#MyChallenge-tab').addClass('active');
+        $('#Favorite-tab').removeClass('active');
+        document.getElementById('MyChallenge').style.display = "";
+        document.getElementById('Favorite').style.display = "none";
+      });
+
+      $("#Favorite-tab").click(function () {
+        $('#MyChallenge-tab').removeClass('active');
+        $('#Favorite-tab').addClass('active');
+        document.getElementById('MyChallenge').style.display = "none";
+        document.getElementById('Favorite').style.display = "";
+      });
+    });
   }
 
 }
